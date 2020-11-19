908d53a244158268ee4804bf2151e0c0108b5beb
pextech <mcstain1639@gmail.com>
2020-11-19 11:33:12 +0200

    ft: dashboard blog what does this PR do> -has a connection with firebase storage and firebase firestore -perfotm CRUd OPERATIONS -retrieve data from firebase

--- js/blog-post.js
+++ js/blog-post.js
@@ -1,6 +1,76 @@
-const reviews = document.querySelector('#reviewItems');
 
-db.collection('reviews').onSnapshot((snapshot)=>{
+
+
+const postForm = document.querySelector('#postForm');
+
+
+postForm.addEventListener("submit",(e)=>{
+
+
+
+   e.preventDefault();
+   let title = document.querySelector('#postTitle');
+   let body = document.querySelector('#postBody');
+   let image = document.querySelector('#image').files[0];
+   let imageName = image.name;
+   
+
+   
+
+   const storageRef = storage.ref(imageName);
+   const uploadTask = storageRef.put(image);
+
+   uploadTask.on('state_changed',(snapshot)=>{
+
+    var progress = (snapshot.bytesTransferred/snapshot.totalBytes)*100;
+
+    console.log('upload is' + progress + "done");
+
+   },(error)=>{
+
+    console.log(error.message);
+
+   },()=>{
+
+    uploadTask.snapshot.ref.getDownloadURL().then((downloadURL)=>{
+
+        savePost(downloadURL);
+
+    })
+   })
+
+    function savePost(url){
+    db.collection('posts').add({
+
+        title: title.value,
+        body: body.value,
+        image: url
+    
+
+    });
+
+    postForm.reset();
+    
+
+    $(document).ready(function(){
+
+       
+            $("#addPage").hide();
+        
+        
+        
+    });
+}
+
+   })
+
+
+
+
+
+ const reviewPost = document.querySelector('.blogContainer');
+
+db.collection('posts').onSnapshot((snapshot)=>{
 
     let changes = snapshot.docChanges();
  
@@ -9,12 +79,12 @@ changes.forEach(change => {
     if(change.type == 'added'){
         
 
-        getReviews(change.doc);
+        getPosts(change.doc);
 
     }
     else if(change.type == 'removed' ){
-        let tr = reviews.querySelector('[data-id='+ change.doc.id + ']');
-        reviews.removeChild(tr);
+        let post = reviewPost.querySelector('[data-id='+ change.doc.id + ']');
+        reviewPost.removeChild(post);
     }
 
 });
@@ -24,49 +94,98 @@ changes.forEach(change => {
 });
 
 
-function getReviews(element){
+function getPosts(element){
 
-    let row  = document.createElement('tr');
-    let email = document.createElement('td');
-    let name = document.createElement('td');
-    let phone = document.createElement('td');
-    let message = document.createElement('td');
+    let post  = document.createElement('div');
+    post.setAttribute('class','blog');
+    let pictureDiv  = document.createElement('div');
+    pictureDiv.setAttribute('class','blogPicture');
+    let image= document.createElement('img');
     
-    let deleteBtn = document.createElement('div');
+    let blogTitle  = document.createElement('div');
+    blogTitle.setAttribute('class','blogTitle');
+    let blogDescription  = document.createElement('div');
+    blogDescription.setAttribute('class','blogDescription');
+    let body  = document.createElement('p');
+    let blogTime  = document.createElement('div');
+    blogTime.setAttribute('class','blogTime');
+    let date  = document.createElement('p');
+   
+    let blogAction  = document.createElement('div');
+    blogAction.setAttribute('class','blogAction');
+    let actionContainer  = document.createElement('div');
+    actionContainer.setAttribute('class','actionContainer');
 
-    row.setAttribute('data-id',element.id);
-    name.textContent = element.data().name;
-    email.textContent = element.data().email;
-    phone.textContent = element.data().phone;
-    message.textContent = element.data().message;
-    deleteBtn.textContent = 'Delete';
+    let view  = document.createElement('div');
+    view.setAttribute('class','view');
+    let viewBtn  = document.createElement('button');
+    viewBtn.setAttribute('class','viewBtn');
+    let edit  = document.createElement('div');
+    edit.setAttribute('class','edit');
+    let editBtn  = document.createElement('button');
+    editBtn.setAttribute('class','editBtn');
+    let delet = document.createElement('div');
+    delet.setAttribute('class','delete');
+    let deleteBtn  = document.createElement('button');
+    deleteBtn.setAttribute('class','deleteBtn');
+   
+    viewBtn.textContent = 'view';
+    deleteBtn.textContent = 'delete';
+    editBtn.textContent = " edit";
 
-    console.log(element.id)
+    
+    // let deleteBtn = document.createElement('div');
 
-    row.appendChild(name);
-    row.appendChild(email);
-    row.appendChild(phone);
-    row.appendChild(message);
-    row.appendChild(deleteBtn);
+    post.setAttribute('data-id',element.id);
+    image.setAttribute('src',element.data().image);
+   
+    blogTitle.textContent = element.data().title;
+    body.textContent = element.data().body;
+    date.textContent = '24 jan 2020';
+    
+    
 
-    reviews.appendChild(row);
+    post.appendChild(pictureDiv);
+    pictureDiv.appendChild(image);
+    post.appendChild(blogTitle);
+    post.appendChild(blogDescription);
+    blogDescription.appendChild(body);
+    post.appendChild(blogTime);
+    blogTime.appendChild(date);
+    post.appendChild(blogAction);
+    blogAction.appendChild(actionContainer);
+    actionContainer.appendChild(view);
+    view.appendChild(viewBtn);
+    actionContainer.appendChild(edit);
+    edit.appendChild(editBtn);
+    actionContainer.appendChild(delet);
+    delet.appendChild(deleteBtn);
+
+   
+   
+
+    reviewPost.appendChild(post);
 
 
     deleteBtn.addEventListener('click',(e)=>{
+
         e.preventDefault();
 
-        let id = e.target.parentElement.getAttribute('data-id');
-        db.collection('reviews').doc(id).delete();
-    
-    });
+        let id = e.target.parentElement.parentElement.parentElement.parentElement.getAttribute('data-id');
+        db.collection('posts').doc(id).delete();
+
+
+    })
+
+   
 
 }
 
    
-         
     
 
 
+       
 
 
 
